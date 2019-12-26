ef3f1e38f45f8443df0b0130189b8fb7dfae300f
toniMR <toni97sk8@gmail.com>
2019-12-26 23:01:30 +0100

    minor changes

--- src/productos/routes/producto_routes.js
+++ src/productos/routes/producto_routes.js
@@ -3,11 +3,11 @@
     en esa ruta
 */
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Controller module
-var producto_controller = require ('../controllers/producto_controller.js');
+const producto_controller = require ('../controllers/producto_controller.js');
 
 
 // ruta /
@@ -24,19 +24,19 @@ router.get('/status', async function(req, res){
 // Rutas para el producto
 
 // GET todos los productos
-router.get('/productos', producto_controller.products_data);
+router.get('/productos', producto_controller.productsData);
 
 // GET un producto concreto (por su id)
-router.get('/productos/:id', producto_controller.product_data);
+router.get('/productos/:_id', producto_controller.productData);
 
 // GET productos que contengan una palabra clave en su nombre o descripción
-router.get('productos/:palabra', producto_controller.product_data_word);
+router.get('productos/:palabra', producto_controller.productDataWord);
 
 // GET productos en un rango de precio
 //router.get('/productos/min/:min/max/:max',      );
 
 // GET productos que pertenezcan a una categoría
-//router.get('/productos/categoria/:categoria',       );
+//router.get('/productos/categoria/:categoria',      );
 
 
 // POST producto
@@ -49,7 +49,5 @@ router.put('/productos/:_id', producto_controller.editProduct);
 router.delete('/productos/:_id',producto_controller.deleteProduct);
 
 
+
 module.exports = router;
-
-
-// app.listen(3003);
