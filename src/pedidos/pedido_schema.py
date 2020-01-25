a4347c3233fd78bca48ef4a2dbcf1be45a498358
toniMR <toni97sk8@gmail.com>
2020-01-25 08:12:52 +0100

    Añadiendo schema de pedidos closes #56

---
+++ src/pedidos/pedido_schema.py
@@ -0,0 +1,22 @@
+from schema import Schema, And, Use
+
+import json
+
+class PedidoSchema:
+    def __init__(self):
+        self.schema = Schema({'id': And(str),
+                                'destinatario': And(str),
+                                'direccion': And(str),
+                                'productos': [{
+                                                    'id': And(str),
+                                                    'unidades': And(int)
+                                            }]
+                                })
+
+    def validate (self, p_json):
+        return self.schema.validate(p_json)
+
+    def is_valid(self, p_json):
+        return self.schema.is_valid(p_json)
+
+    
