1b42d9e125e2f7c379d6152f2c431f0eabcc9cf7
toniMR <toni97sk8@gmail.com>
2020-02-02 01:09:29 +0100

    Añadir tests insertar Pedido closes #21

---
+++ test/pedidos/test_pedidoRest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import unittest
+import json
+import sys, os.path
+
+# Añadir la ruta de los módulos 
+src_path = (os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
++ '/src/pedidos/')
+sys.path.append(src_path)
+
+from pedidos_rest import app
+
+class TestPedidosRest (unittest.TestCase):
+
+    
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_01_insertar_pedido(self):
+        pedido_test =  {"id": "PYTEST", "destinatario": "Antonio", "direccion": "c/testing", "productos": [{"id": "MI200","unidades": 4},{"id": "LAM3","unidades": 7}]}
+        result = self.app.post('pedidos', json=pedido_test)
+        self.assertEqual(result.status_code, 201)
+
+    def test_02_insertar_pedido_malformado(self):
+        pedido_test =  {"id": "PYTEST2", "dest": "Antonio", "dir": "c/testing", "prods": [{"id": "MI200","unids": 4},{"id": "LAM3","unids": 7}]}
+        result = self.app.post('pedidos', json=pedido_test)
+        self.assertEqual(result.status_code, 400)
+
+    def test_03_insertar_pedido_existente(self):
+        pedido_test =  {"id": "PYTEST", "destinatario": "Antonio", "direccion": "c/testing", "productos": [{"id": "MI200","unidades": 4},{"id": "LAM3","unidades": 7}]}
+        result = self.app.post('pedidos', json=pedido_test)
+        self.assertEqual(result.status_code, 400)
+
